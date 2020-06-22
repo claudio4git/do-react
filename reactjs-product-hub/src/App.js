554883b78d14d1a006d17cf045d79a3f9fd63fcc
claudio.costa <claudio.costa@invillia.com>
2020-06-22 15:13:09 -0300

    Product hub implementation

--- reactjs-product-hub/src/App.js
+++ reactjs-product-hub/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Routes from "./routes";
-
-import "./styles.css"
-
 import Header from './components/Header';
+import "./styles.css"
 
 const App = () => (
   <div className="App">
